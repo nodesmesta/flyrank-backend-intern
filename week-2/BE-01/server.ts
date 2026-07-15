adf3946b0f8888e73dc586af7b5a0f25593cbcf0
Nodesemesta <nodesemesta@users.noreply.github.com>
2026-07-15 11:33:43 +0700

    Stage 5: Swagger UI

--- week-2/BE-01/server.ts
+++ week-2/BE-01/server.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import { readFileSync } from "fs";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+const openapiDoc = JSON.parse(
+  readFileSync(new URL("openapi.json", import.meta.url), "utf-8"),
+);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiDoc));
 
 interface Task {
   id: number;
